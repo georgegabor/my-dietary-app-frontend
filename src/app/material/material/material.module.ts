4cf6684047abfdb03fdc5e20b5672003f33646f7
georgegabor <georgegabor@rocketmail.com>
2022-02-20 18:01:21 +0100

    Add image, style, fields

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatGridListModule } from '@angular/material/grid-list'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 import { MatListModule } from '@angular/material/list'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatPaginatorModule } from '@angular/material/paginator'
@@ -32,6 +34,9 @@ const MAT = [
   MatPaginatorModule,
   MatProgressBarModule,
   MatSortModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
 ]
 
 @NgModule({
--- src/app/modules/form/form.component.ts
+++ src/app/modules/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
@@ -6,11 +7,65 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`
+  myForm: FormGroup
 
-  constructor() {}
+  countryList = [
+    { id: '1', name: 'Austria' },
+    { id: '2', name: 'Germany' },
+    { id: '3', name: 'France' },
+  ]
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(15)]],
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: [''],
+      }),
+      aliases: this.fb.array([this.fb.control('')]),
+    })
+  }
 
   ngOnInit() {}
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid) // true or false
+    console.log('Name', form.value.name)
+    console.log('Email', form.value.email)
+    console.log('Message', form.value.message)
+    console.log('Address', form.value.address)
+    console.log('Aliases', form.value.aliases)
+  }
+
+  addName() {
+    this.myForm.get('name')?.setValue('Sammy')
+  }
+
+  addEmail() {
+    this.myForm.get('email')?.setValue('sammy@email.com')
+  }
+
+  addMessage() {
+    this.myForm.get('message')?.setValue('Sammy has a long message for you')
+  }
+
+  likeClicked() {
+    window.alert('likeClicked')
+  }
+
+  shareClicked() {
+    window.alert('shareClicked')
+  }
+
+  get aliases() {
+    return this.myForm.get('aliases') as FormArray
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''))
+  }
 }
