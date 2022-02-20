6074b1ab9915aa882f4f0edf9a03cd18a43583ad
georgegabor <georgegabor@rocketmail.com>
2022-02-20 18:32:52 +0100

    Add dropdown

--- src/app/modules/form/form.component.ts
+++ src/app/modules/form/form.component.ts
@@ -9,10 +9,14 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class FormComponent implements OnInit {
   myForm: FormGroup
 
-  countryList = [
-    { id: '1', name: 'Austria' },
-    { id: '2', name: 'Germany' },
-    { id: '3', name: 'France' },
+  teams: any[] = [
+    { name: 'Liverpool' },
+    { name: 'Manchester City' },
+    { name: 'Manchester United' },
+    { name: 'Arsenal' },
+    { name: 'Leicester City' },
+    { name: 'Chelsea' },
+    { name: 'Tottenham Hotspur' },
   ]
 
   constructor(private fb: FormBuilder) {
@@ -20,6 +24,7 @@ export class FormComponent implements OnInit {
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required, Validators.minLength(15)]],
+      team: [''],
       address: this.fb.group({
         street: [''],
         city: [''],
@@ -37,6 +42,7 @@ export class FormComponent implements OnInit {
     console.log('Name', form.value.name)
     console.log('Email', form.value.email)
     console.log('Message', form.value.message)
+    console.log('Team', form.value.team)
     console.log('Address', form.value.address)
     console.log('Aliases', form.value.aliases)
   }
