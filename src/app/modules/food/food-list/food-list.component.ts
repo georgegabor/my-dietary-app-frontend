8719c46a18055098ef5c1c0faec3847ff9659619
georgegabor <georgegabor@rocketmail.com>
2022-03-19 15:16:08 +0100

    Add list

--- src/app/modules/food/food-list/food-list.component.ts
+++ src/app/modules/food/food-list/food-list.component.ts
@@ -13,26 +13,6 @@ import { Subscription } from 'rxjs';
 import { Food } from './../model/Food';
 import { FoodService } from './../service/food.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-food',
   templateUrl: './food-list.component.html',
@@ -44,28 +24,29 @@ export class FoodListComponent implements OnInit, OnDestroy {
   @ViewChild(MatSort) sort!: MatSort;
 
   subscription = new Subscription();
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<Food>([]);
 
-  displayedColumns: string[] = [];
+  displayedColumns = [
+    'name',
+    'energyInKcal',
+    'totalFat',
+    'saturatedFat',
+    'totalCarbohydrate',
+    'sugar',
+    'protein',
+    'salt',
+  ];
 
   tables = [0];
   constructor(
     private readonly foodService: FoodService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.displayedColumns.length = 4;
-
-    // The first two columns should be position and name; the last two columns: weight, symbol
-    this.displayedColumns[0] = 'position';
-    this.displayedColumns[1] = 'name';
-    this.displayedColumns[2] = 'weight';
-    this.displayedColumns[3] = 'symbol';
-  }
+  ) {}
 
   ngOnInit() {
     this.subscription = this.foodService.foodList$.subscribe((res: Food[]) => {
-      console.log(res);
+      this.dataSource.data = res;
     });
   }
 
