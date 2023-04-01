c054b42264af857640de2c57f2a862c129513977
georgegabor <georgegabor@rocketmail.com>
2023-04-01 14:58:21 +0200

    Add window toggle

--- src/app/modules/reactive-component/reactive-component.component.ts
+++ src/app/modules/reactive-component/reactive-component.component.ts
@@ -1,18 +1,37 @@
 import { outerActions, TableAction } from './../table-extends-subject/table-extends-subject.component';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { filter, tap } from 'rxjs';
+import { BehaviorSubject, filter, mergeAll, NEVER, Subject, take, tap, timer, windowToggle } from 'rxjs';
 import { InnerActions, TableExtendsSubjectComponent } from '../table-extends-subject/table-extends-subject.component';
 
 @Component({
   selector: 'app-reactive-component',
   template: `
-    <p>ReactiveComponent</p>
-    <table-extends-subject></table-extends-subject>
+    <div>
+      <p>ReactiveComponent</p>
+      <table-extends-subject></table-extends-subject>
+    </div>
+
+    <div style="display: flex; justify-content: center;">
+      <button mat-button color="primary" (click)="on$.next({})">ON</button>
+      <button mat-button color="primary" (click)="off$.next()">OFF</button>
+    </div>
   `,
   styleUrls: ['./reactive-component.component.scss'],
 })
 export class ReactiveComponentComponent implements AfterViewInit {
-  constructor() {}
+  constructor() {
+    const source$ = timer(0, 1000).pipe(take(50));
+
+    source$
+      .pipe(
+        windowToggle(this.on$, () => this.off$),
+        mergeAll()
+      )
+      .subscribe(console.log);
+  }
+
+  on$ = new BehaviorSubject<any>({});
+  off$ = new Subject<void>();
 
   @ViewChild(TableExtendsSubjectComponent)
   private _table: TableExtendsSubjectComponent;
