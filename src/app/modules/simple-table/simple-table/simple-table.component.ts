27ea4f77350f52d9b8d521bf26e5868818b2e1ad
georgegabor <georgegabor@rocketmail.com>
2023-04-06 15:25:11 +0200

    Add Record type example

--- src/app/modules/simple-table/simple-table/simple-table.component.ts
+++ src/app/modules/simple-table/simple-table/simple-table.component.ts
@@ -1,28 +1,36 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { generate, interval, take, tap } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+interface TableState {
+  state: State;
+  data: any[];
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+const initialState: TableState = {
+  state: 'read',
+  data: [{ type: 'initial' }],
+};
+
+type State = 'read' | 'edit' | 'create';
+type ActionType = 'edit' | 'create' | 'cancel';
+type Action = (value: TableState) => void;
+
+const record: Record<ActionType, Action> = {
+  edit: (value) =>
+    Object.assign(value, {
+      data: [...value.data, { type: 'edited' }],
+      state: 'edit',
+    }),
+  create: (value) =>
+    Object.assign(value, {
+      data: [...value.data, { type: 'created' }],
+      state: 'create',
+    }),
+  cancel: (value) =>
+    Object.assign(value, {
+      data: [...value.data, { type: 'cancelled' }],
+      state: 'read',
+    }),
+};
 
 @Component({
   selector: 'app-simple-table',
@@ -30,63 +38,12 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./simple-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SimpleTableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  // dataSource = new MatTableDataSource();
-  dataSource = new MatTableDataSource(ELEMENT_DATA.slice(0, 4));
-
+export class SimpleTableComponent {
   constructor(public cdf: ChangeDetectorRef) {}
 
-  ngOnInit() {}
+  state = initialState;
 
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  gen$ = generate(
-    1,
-    (x) => x < 100,
-    (x) => x * 2
-  ).pipe(tap(console.log));
-
-  ngAfterViewInit() {
-    // const obs$ = interval().pipe(
-    //   take(500),
-    //   // tap(console.log),
-    //   tap((v) => {
-    //     const ds = new MatTableDataSource();
-    //     ds.sort = this.sort;
-    //     ds.paginator = this.paginator;
-    //     ds.data = [...this.dataSource.data, ELEMENT_DATA[v % 10]];
-
-    //     this.dataSource = ds;
-    //   })
-    // );
-
-    // obs$.subscribe();
-
-    const obs$ = interval(10000).pipe(
-      take(1),
-      // tap(console.log),
-      tap((v) => {
-        const ds = new MatTableDataSource<PeriodicElement>();
-        ds.sort = this.sort;
-        ds.paginator = this.paginator;
-        console.log(v + ' ' + (9 - v));
-        this.dataSource.data[v] = { ...ELEMENT_DATA[9 - v] };
-        ds.data = [...this.dataSource.data];
-
-        // console.log(this.dataSource.data[v]);
-        this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA.slice(4));
-        // this.cdf.markForCheck();
-        // console.log(this.dataSource.data[v]);
-      })
-    );
-
-    // obs$.subscribe();
-    this.gen$.subscribe();
-  }
-
-  announceSortChange(sortState: Sort) {
-    console.log(sortState);
+  onClick(value: ActionType) {
+    record[value](this.state);
   }
 }
